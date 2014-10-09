cca1d150dc6bb6a503c4e56854a6b225d0633f7e
Michael Heuer (NMDP) <mheuer@nmdp.org>
2014-10-09 12:25:50 -0500

    adding filter-interpretable-exons tool

---
+++ align/src/main/java/org/nmdp/ngs/align/GenewiseExon.java
@@ -0,0 +1,101 @@
+/*
+
+    ngs-align  Sequence alignment.
+    Copyright (c) 2014 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.gnu.org/licenses/lgpl.html
+
+*/
+package org.nmdp.ngs.align;
+
+import com.google.common.collect.Range;
+
+/**
+ * Genewise exon.
+ */
+public final class GenewiseExon {
+    private final long start;
+    private final long end;
+    private final int phase;
+    private final Range<Long> range;
+
+
+    /**
+     * Create a new genewise exon.
+     *
+     * @param start start, in 1-based coordinate system, fully closed range
+     * @param end end, in 1-based coordinate system, fully closed range
+     * @param phase phase
+     */
+    GenewiseExon(final long start, final long end, final int phase) {
+        this.start = start;
+        this.end = end;
+        this.phase = phase;
+        range = Range.closed(start, end);
+    }
+
+
+    /**
+     * Return the start of this genewise exon in 1-based coordinate system, fully closed range.
+     *
+     * @return the start of this genewise exon in 1-based coordinate system, fully closed range
+     */
+    public long start() {
+        return start;
+    }
+
+    /**
+     * Return the start of this genewise exon in 1-based coordinate system, fully closed range.
+     *
+     * @return the start of this genewise exon in 1-based coordinate system, fully closed range
+     */
+    public long end() {
+        return end;
+    }
+
+    /**
+     * Return the phase of this genewise exon.
+     *
+     * @return the phase of this genewise exon
+     */
+    public int phase() {
+        return phase;
+    }
+
+    /**
+     * Return this genewise exon as a 1-based coordinate system, fully closed range.
+     *
+     * @return this genewise exon as a 1-based coordinate system, fully closed range
+     */
+    public Range<Long> asRange() {
+        return range;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Exon ");
+        sb.append(start);
+        sb.append(" ");
+        sb.append(end);
+        sb.append(" phase ");
+        sb.append(phase);
+        sb.append(" range ");
+        sb.append(range);
+        return sb.toString();
+    }
+}
---
+++ tools/src/main/java/org/nmdp/ngs/tools/FilterInterpretableExons.java
@@ -0,0 +1,173 @@
+/*
+
+    ngs-tools  Next generation sequencing (NGS/HTS) command line tools.
+    Copyright (c) 2014 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.gnu.org/licenses/lgpl.html
+
+*/
+package org.nmdp.ngs.tools;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import static org.dishevelled.compress.Readers.reader;
+import static org.dishevelled.compress.Writers.writer;
+
+import static org.nmdp.ngs.align.Genewise.genewiseExons;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.biojava.bio.BioException;
+
+import org.biojava.bio.seq.Sequence;
+import org.biojava.bio.seq.SequenceIterator;
+
+import org.biojava.bio.seq.io.SeqIOTools;
+
+import org.dishevelled.commandline.ArgumentList;
+import org.dishevelled.commandline.CommandLine;
+import org.dishevelled.commandline.CommandLineParseException;
+import org.dishevelled.commandline.CommandLineParser;
+import org.dishevelled.commandline.Switch;
+import org.dishevelled.commandline.Usage;
+
+import org.dishevelled.commandline.argument.FileArgument;
+
+import org.nmdp.ngs.align.GenewiseExon;
+
+/**
+ * Filter interpretable exons from assembly consensus sequences.
+ */
+public final class FilterInterpretableExons implements Runnable {
+    private final File aminoAcidHmm2File;
+    private final File inputFastaFile;
+    private final File outputFastaFile;
+    private static final String USAGE = "java FilterInterpretableExons -a A_prot.hmm2 -i consensus.fasta -o filtered.fasta";
+
+
+    /**
+     * Filter interpretable exons from assembly consensus sequences.
+     *
+     * @param aminoAcidHmm2File amino acid HMM file in HMMER2 format, must not be null
+     * @param inputFastaFile input FASTA file, if any
+     * @param outputFastaFile output FASTA file, if any
+     */
+    public FilterInterpretableExons(final File aminoAcidHmm2File, final File inputFastaFile, final File outputFastaFile) {
+        checkNotNull(aminoAcidHmm2File);
+        this.aminoAcidHmm2File = aminoAcidHmm2File;
+        this.inputFastaFile = inputFastaFile;
+        this.outputFastaFile = outputFastaFile;
+    }
+
+
+    @Override
+    public void run() {
+        BufferedReader reader = null;
+        PrintWriter writer = null;
+        try {
+            reader = reader(inputFastaFile);
+            writer = writer(outputFastaFile);
+
+            for (SequenceIterator sequences = SeqIOTools.readFastaDNA(reader); sequences.hasNext(); ) {
+                Sequence sequence = sequences.nextSequence();
+                writer.print(">" + sequence.getName());
+
+                StringBuilder sb = new StringBuilder();
+                File tmp = writeSequenceToTempFile(sequence);
+                for (GenewiseExon exon : genewiseExons(aminoAcidHmm2File, tmp)) {
+                    writer.print(":" + exon);
+                    // int precision should be ok here
+                    sb.append(sequence.subStr((int) exon.start(), (int) exon.end()));
+                }
+                writer.print("\n");
+                writer.println(sb.toString());
+            }
+        }
+        catch (BioException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        finally {
+            try {
+                reader.close();
+            }
+            catch (Exception e) {
+                // empty
+            }
+            try {
+                writer.close();
+            }
+            catch (Exception e) {
+                // empty
+            }
+        }
+    }
+
+    /**
+     * Write the specified sequence to a temporary file in FASTA format.
+     *
+     * @param sequence sequence to write
+     * @return temporary file in FASTA format
+     * @throws IOException if an I/O error occurs
+     */
+    static File writeSequenceToTempFile(final Sequence sequence) throws IOException {
+        File tmp = File.createTempFile(sequence.getName() + "-", ".fa");
+        try (FileOutputStream outputStream = new FileOutputStream(tmp)) {
+            SeqIOTools.writeFasta(outputStream, sequence);
+        }
+        return tmp;
+    }
+
+
+    /**
+     * Main.
+     *
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
+        Switch help = new Switch("h", "help", "display help message");
+        FileArgument aminoAcidHmm2File = new FileArgument("a", "amino-acid-hmm2-file", "amino acid HMM file in HMMER2 format", true);
+        FileArgument inputFastaFile = new FileArgument("i", "input-fasta-file", "input FASTA file, default stdin", false);
+        FileArgument outputFastaFile = new FileArgument("o", "output-fasta-file", "output FASTA file, default stdout", false);
+
+        ArgumentList arguments = new ArgumentList(help, aminoAcidHmm2File, inputFastaFile, outputFastaFile);
+        CommandLine commandLine = new CommandLine(args);
+        try {
+            CommandLineParser.parse(commandLine, arguments);
+            if (help.wasFound()) {
+                Usage.usage(USAGE, null, commandLine, arguments, System.out);
+                System.exit(-2);
+            }
+            new FilterInterpretableExons(aminoAcidHmm2File.getValue(), inputFastaFile.getValue(), outputFastaFile.getValue()).run();
+        }
+        catch (CommandLineParseException | NullPointerException e) {
+            Usage.usage(USAGE, e, commandLine, arguments, System.err);
+            System.exit(-1);
+        }
+    }
+}
