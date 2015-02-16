d576cdb89aaadba9a5da8ebf3012669905f17196
Mike Halagan <mhalagan09@gmail.com>
2015-02-16 14:55:30 -0600

    Working version of ngs-extract-expected

--- tools/src/main/java/org/nmdp/ngs/tools/ExtractExpected.java
+++ tools/src/main/java/org/nmdp/ngs/tools/ExtractExpected.java
@@ -27,15 +27,8 @@ import static org.dishevelled.compress.Writers.writer;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.PrintWriter;
-
+import java.util.List;
 import java.util.concurrent.Callable;
-
-import javax.xml.bind.JAXBElement;
-
-import org.biojava.bio.seq.DNATools;
-
-import org.biojava.bio.symbol.SymbolList;
 
 import org.dishevelled.commandline.ArgumentList;
 import org.dishevelled.commandline.CommandLine;
@@ -43,25 +36,22 @@ import org.dishevelled.commandline.CommandLineParseException;
 import org.dishevelled.commandline.CommandLineParser;
 import org.dishevelled.commandline.Switch;
 import org.dishevelled.commandline.Usage;
-
 import org.dishevelled.commandline.argument.FileArgument;
-
 import org.nmdp.ngs.hml.HmlReader;
-
-import org.nmdp.ngs.hml.jaxb.ConsensusSequence;
-import org.nmdp.ngs.hml.jaxb.ConsensusSequenceBlock;
+import org.nmdp.ngs.hml.jaxb.Glstring;
+import org.nmdp.ngs.hml.jaxb.AlleleAssignment;
 import org.nmdp.ngs.hml.jaxb.Hml;
-import org.nmdp.ngs.hml.jaxb.ReferenceDatabase;
 import org.nmdp.ngs.hml.jaxb.Sample;
-import org.nmdp.ngs.hml.jaxb.Sequence;
 import org.nmdp.ngs.hml.jaxb.Typing;
+
+import com.google.common.base.Splitter;
 
 /**
  * Extract consensus sequences from a file in HML format.
  */
-public final class ExtractConsensus implements Callable<Integer> {
+public final class ExtractExpected implements Callable<Integer> {
     private final File inputHmlFile;
-    private static final String USAGE = "ngs-extract-consensus [args]";
+    private static final String USAGE = "ngs-extract-expected [args]";
 
 
     /**
@@ -69,7 +59,7 @@ public final class ExtractConsensus implements Callable<Integer> {
      *
      * @param inputHmlFile input HML file, if any
      */
-    public ExtractConsensus(final File inputHmlFile) {
+    public ExtractExpected(final File inputHmlFile) {
         this.inputHmlFile = inputHmlFile;
     }
 
@@ -78,42 +68,32 @@ public final class ExtractConsensus implements Callable<Integer> {
     public Integer call() throws Exception {
         BufferedReader reader = null;
         try {
+
             reader = reader(inputHmlFile);
             Hml hml = HmlReader.read(reader);
             for (Sample sample : hml.getSample()) {
                 String id = sample.getId();
                 for (Typing typing : sample.getTyping()) {
-                    for (ConsensusSequence consensusSequence : typing.getConsensusSequence()) {
-                        int cons = 0;
-                        try (PrintWriter writer = writer(new File(id + "_" + cons + ".fa.gz"))) {
-                            int blocks = 0;
-                            for (ConsensusSequenceBlock consensusSequenceBlock : consensusSequence.getConsensusSequenceBlock()) {
-                                StringBuilder sb = new StringBuilder(1200);
-                                sb.append(">");
-                                sb.append(blocks);
-                                sb.append("|");
-                                sb.append(((ReferenceDatabase.ReferenceSequence) consensusSequenceBlock.getReferenceSequenceId()).getId());
-                                sb.append("|");
-                                sb.append(consensusSequenceBlock.getStart() == null ? "" : consensusSequenceBlock.getStart());
-                                sb.append("|");
-                                sb.append(consensusSequenceBlock.getPhasingGroup() == null ? "" : consensusSequenceBlock.getPhasingGroup());
-                                sb.append("|");
-                                sb.append(consensusSequenceBlock.getExpectedCopyNumber() == null ? "" : consensusSequenceBlock.getExpectedCopyNumber());
-                                sb.append("|");
-                                sb.append(consensusSequenceBlock.getVariant().isEmpty() ? "1" : "0");
-                                sb.append("|");
-                                sb.append((consensusSequenceBlock.isContinuity() != null && consensusSequenceBlock.isContinuity().booleanValue()) ? "1" : "0");
-                                sb.append("\n");
+                   for (AlleleAssignment alleleAssignment : typing.getAlleleAssignment()) {
+                	   for (Object glstring : alleleAssignment.getPropertyAndHaploidAndGenotypeList()){
 
-                                // todo: move to HlaUtils
-                                SymbolList symbolList = DNATools.createDNA(consensusSequenceBlock.getSequence().getValue().replaceAll("\\s+", ""));
-                                sb.append(symbolList.seqString());
-                                writer.println(sb.toString());
+                          Glstring gl = (Glstring)glstring;
+                          List<String> Alleles = Splitter.onPattern("[+]+").splitToList(gl.getValue().replaceAll("[\n\r ]", ""));                          
+                          
+                          String locus = Alleles.get(0).substring(Alleles.get(0).indexOf("-") - 3, Alleles.get(0).indexOf("*"));
+                          int zygosity = (Alleles.get(0).equals(Alleles.get(1))) ? 1 : 2;
+                          
+                          String ars = (locus.equals("HLA-A")) ? "./tutorial/regions/grch38/hla-a/hla-a.ars.txt" :
+                        	  (locus.equals("HLA-B")) ? "./tutorial/regions/grch38/hla-b/hla-b.ars.txt" : 
+                        	  (locus.equals("HLA-C")) ? "./tutorial/regions/grch38/hla-c/hla-a.ars.txt" : 	  
+                              (locus.equals("HLA-DRB1")) ? "./tutorial/regions/grch38/hla-drb1/hla-drb1.ars.txt" : 	  
+                              (locus.equals("HLA-DQB1")) ? "./tutorial/regions/grch38/hla-dqb1/hla-dqb1.ars.txt" : 
+                              (locus.equals("HLA-DPB1")) ? "./tutorial/regions/grch38/hla-dpb1/hla-dpb1.ars.txt" : "N/A";                            	  
 
-                                blocks++;
-                            }
+                          System.out.print(id + ".fastq.contigs.bwa.sorted.bam\t" + locus + "\t" + ars + "\t" + zygosity + "\t" + Alleles.get(0) + "\t" + Alleles.get(1) + "\n");
+
+                          
                         }
-                        cons++;
                     }
                 }
             }
@@ -144,7 +124,7 @@ public final class ExtractConsensus implements Callable<Integer> {
         ArgumentList arguments = new ArgumentList(about, help, inputHmlFile);
         CommandLine commandLine = new CommandLine(args);
 
-        ExtractConsensus extractConsensus = null;
+        ExtractExpected extractExpected = null;
         try
         {
             CommandLineParser.parse(commandLine, arguments);
@@ -156,14 +136,14 @@ public final class ExtractConsensus implements Callable<Integer> {
                 Usage.usage(USAGE, null, commandLine, arguments, System.out);
                 System.exit(0);
             }
-            extractConsensus = new ExtractConsensus(inputHmlFile.getValue());
+            extractExpected = new ExtractExpected(inputHmlFile.getValue());
         }
         catch (CommandLineParseException | IllegalArgumentException e) {
             Usage.usage(USAGE, e, commandLine, arguments, System.err);
             System.exit(-1);
         }
         try {
-            System.exit(extractConsensus.call());
+            System.exit(extractExpected.call());
         }
         catch (Exception e) {
             e.printStackTrace();
