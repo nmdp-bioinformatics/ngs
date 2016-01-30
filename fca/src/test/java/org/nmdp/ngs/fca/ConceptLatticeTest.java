94f2e303e26a3b70732bdd9421a99ec0811554d0
Caleb Kennedy <int33484@L0504211.local>
2016-01-30 15:45:50 -0600

    Remove tests that presume a specific ordering of elements in array

--- fca/src/test/java/org/nmdp/ngs/fca/ConceptLatticeTest.java
+++ fca/src/test/java/org/nmdp/ngs/fca/ConceptLatticeTest.java
@@ -37,26 +37,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit test for ConceptLattice.
+ * Unit tests for ConceptLattice.
  */
 public final class ConceptLatticeTest {
     private ConceptLattice lattice;
-    private List STUVWX, abcdefg, abdefg, abde, abdf, acef, bd, af;
-
+    
     /*
      * Example taken from Davey and Priestly "Introduction to Lattices and Order"
      * second edition, p 77.
      */
     @Before
     public void setUp() {
-        abcdefg = list("a", "b", "c", "d", "e", "f", "g");
-        abdefg = list("a", "b", "d", "e", "f", "g");
-        abde = list("a", "b", "d", "e");
-        abdf = list("a", "b", "d", "f");
-        acef = list("a", "c", "e", "f");
-        bd = list("b", "d");
-        af = list("a", "f");
-
         lattice = new ConceptLattice(new TinkerGraph(), 7);
 
         lattice.insert(new Concept(bits(0), bits(0, 1, 3, 5)));
@@ -65,8 +56,6 @@ public final class ConceptLatticeTest {
         lattice.insert(new Concept(bits(3), bits(0, 2, 4, 5)));
         lattice.insert(new Concept(bits(4), bits(1, 3)));
         lattice.insert(new Concept(bits(5), bits(0, 5)));
-
-        STUVWX = list("S", "T", "U", "V", "W", "X");
     }
 
     @Test
@@ -84,8 +73,8 @@ public final class ConceptLatticeTest {
 
     @Test
     public void testMeet() {
-        Concept left = (Concept) lattice.meet(lattice.top(), new Concept(bits(), bits(1, 3)));// Concept.builder().withAttributes(bd, abcdefg).build());
-        Concept right = (Concept) lattice.meet(lattice.top(), new Concept(bits(), bits(0, 5))); //Concept.builder().withAttributes(af, abcdefg).build());
+        Concept left = (Concept) lattice.meet(lattice.top(), new Concept(bits(), bits(1, 3)));
+        Concept right = (Concept) lattice.meet(lattice.top(), new Concept(bits(), bits(0, 5)));
 
         assertEquals(left.intent(), bits(1, 3));
         assertEquals(right.intent(), bits(0, 5));
@@ -95,8 +84,8 @@ public final class ConceptLatticeTest {
 
     @Test
     public void testJoin() {
-        Concept left = (Concept) lattice.join(lattice.bottom(), new Concept(bits(), bits(1, 3)));//Concept.builder().withAttributes(bd, abcdefg).build());
-        Concept right = (Concept) lattice.join(lattice.bottom(), new Concept(bits(), bits(0, 5))); //Concept.builder().withAttributes(af, abcdefg).build());
+        Concept left = (Concept) lattice.join(lattice.bottom(), new Concept(bits(), bits(1, 3)));
+        Concept right = (Concept) lattice.join(lattice.bottom(), new Concept(bits(), bits(0, 5)));
 
         assertEquals(left.intent(), bits(1, 3));
         assertEquals(right.intent(), bits(0, 5));
@@ -106,9 +95,9 @@ public final class ConceptLatticeTest {
 
     @Test
     public void testMeasure() {
-        Concept V = new Concept(bits(3), bits(0, 2, 4, 5)); //Concept.builder().withObjects(list("V"), STUVWX).withAttributes(acef, abcdefg).build();
-        Concept W = new Concept(bits(4), bits(1, 3)); // Concept.builder().withObjects(list("W"), STUVWX).withAttributes(bd, abcdefg).build();
-        Concept Z = new Concept(bits(6), bits(0)); //Concept.builder().withObjects(list("Z"), STUVWX).withAttributes(list("a"), abcdefg).build();
+        Concept V = new Concept(bits(3), bits(0, 2, 4, 5));
+        Concept W = new Concept(bits(4), bits(1, 3));
+        Concept Z = new Concept(bits(6), bits(0));
 
         assertEquals(0.0, lattice.measure(V, W), 0.0);
         assertEquals(0.0, lattice.measure(W, V), 0.0);
@@ -132,7 +121,5 @@ public final class ConceptLatticeTest {
     public void testToArray() {
         Object[] concepts = lattice.toArray();
         assertEquals(concepts.length, 12);
-        assertEquals(concepts[0], new Concept(bits(), bits(0, 1, 2, 3, 4, 5, 6)));
-        assertEquals(concepts[11], new Concept(bits(2, 3), bits(0, 4, 5)));
     }
 }
